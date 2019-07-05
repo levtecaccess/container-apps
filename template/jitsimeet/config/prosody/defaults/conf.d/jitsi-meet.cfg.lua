ce6ef346b46643cbe83a6e91f6d5b64e3ac33684
levtecaccess <levijohnson2008@live.co.uk>
2019-07-05 07:00:52 +0100

    Add "jitsimeet" application + Fixes
    Added the "jitsimeet" application
    Resolved various installation wizard issues for the "roundcube" application

---
+++ template/jitsimeet/config/prosody/defaults/conf.d/jitsi-meet.cfg.lua
@@ -0,0 +1,59 @@
+admins = { "{{ .Env.JICOFO_AUTH_USER }}@{{ .Env.XMPP_AUTH_DOMAIN }}" }
+plugin_paths = { "/prosody-plugins-custom" }
+http_default_host = "{{ .Env.XMPP_DOMAIN }}"
+
+VirtualHost "{{ .Env.XMPP_DOMAIN }}"
+    {{ if .Env.ENABLE_AUTH | default "0" | toBool }}
+    authentication = "internal_plain"
+    {{ else }}
+    authentication = "anonymous"
+    {{ end }}
+    ssl = {
+        key = "/config/certs/{{ .Env.XMPP_DOMAIN }}.key";
+        certificate = "/config/certs/{{ .Env.XMPP_DOMAIN }}.crt";
+    }
+    modules_enabled = {
+        "bosh";
+        "pubsub";
+        "ping";
+        {{ if .Env.XMPP_MODULES }}
+        "{{ join "\";\n\"" (splitList "," .Env.XMPP_MODULES) }}";
+        {{ end }}
+    }
+
+    c2s_require_encryption = false
+
+{{ if and (.Env.ENABLE_AUTH | default "0" | toBool) (.Env.ENABLE_GUESTS | default "0" | toBool) }}
+VirtualHost "{{ .Env.XMPP_GUEST_DOMAIN }}"
+    authentication = "anonymous"
+    c2s_require_encryption = false
+{{ end }}
+
+VirtualHost "{{ .Env.XMPP_AUTH_DOMAIN }}"
+    ssl = {
+        key = "/config/certs/{{ .Env.XMPP_AUTH_DOMAIN }}.key";
+        certificate = "/config/certs/{{ .Env.XMPP_AUTH_DOMAIN }}.crt";
+    }
+    authentication = "internal_plain"
+
+Component "{{ .Env.XMPP_INTERNAL_MUC_DOMAIN }}" "muc"
+    modules_enabled = {
+        "ping";
+        {{ if .Env.XMPP_INTERNAL_MUC_MODULES }}
+        "{{ join "\";\n\"" (splitList "," .Env.XMPP_INTERNAL_MUC_MODULES) }}";
+        {{ end }}
+    }
+    storage = "memory"
+    muc_room_cache_size = 1000
+
+Component "{{ .Env.XMPP_MUC_DOMAIN }}" "muc"
+    storage = "memory"
+    modules_enabled = {
+        {{ if .Env.XMPP_MUC_MODULES }}
+        "{{ join "\";\n\"" (splitList "," .Env.XMPP_MUC_MODULES) }}";
+        {{ end }}
+    }
+
+Component "focus.{{ .Env.XMPP_DOMAIN }}"
+    component_secret = "{{ .Env.JICOFO_COMPONENT_SECRET }}"
+
---
+++ template/jitsimeet/wizard/description/eng.md
@@ -0,0 +1,16 @@
+Jitsi Meet
+More secure, more flexible, and completely free video conferencing.
+
+Go ahead, video chat with the whole team. In fact, invite everyone you know. Jitsi Meet is a fully encrypted, 100% open source video conferencing solution that you can use all day, every day, for free — with no account needed.
+
+What else can you do with Jitsi Meet?
+• Share your desktop, presentations, and more
+• Invite users to a conference via a simple, custom URL
+• Edit documents together using Etherpad
+• Pick fun meeting URLs for every meeting
+• Trade messages and emojis while you video conference, with integrated chat.
+
+## Prerequisites
+To run Jitsi Meet, a minimum of 4 GB of available RAM is recommended for your Docker host. For more information about Jitsi Meet hardware requirements, go to <a href="https://jitsi.org/jitsi-meet/" target="_blank">Jitsi Meet - Project Page</a>
+
+Note: Jitsi Meet may need a few minutes to start.
